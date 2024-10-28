bc3f371cd49ce2520688b3be4f7c79ce67ba678a
Liam O'Hara <liam.ohara@gmail.com>
2024-10-28 17:06:55 +0000

    Feat: add Player class.

---
+++ src/main/java/liamohara/model/Player.java
@@ -0,0 +1,13 @@
+package liamohara.model;
+
+public class Player {
+    int movesRemaining = 3;
+
+    public int getMovesRemaining() {
+        return movesRemaining;
+    }
+
+    public void setMovesRemaining(int movesRemaining) {
+        this.movesRemaining = movesRemaining;
+    }
+}
