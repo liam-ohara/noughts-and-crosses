4b65f59b3ade2f7b6c0fbecfed9a6c91dcd77f70
Liam O'Hara <liam.ohara@gmail.com>
2024-10-28 17:10:30 +0000

    Feat: implement playerName and playerScore fields and setters and getters. Add playerName to constructor.

--- src/main/java/liamohara/model/Player.java
+++ src/main/java/liamohara/model/Player.java
@@ -1,13 +1,38 @@
 package liamohara.model;
 
 public class Player {
+
+    String playerName;
+
     int movesRemaining = 3;
+
+    int playerScore = 0;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
 
     public int getMovesRemaining() {
         return movesRemaining;
     }
 
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
     public void setMovesRemaining(int movesRemaining) {
         this.movesRemaining = movesRemaining;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
     }
 }
