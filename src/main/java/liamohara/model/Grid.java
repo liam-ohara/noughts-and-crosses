90848933334fc33bdea35b63dd8b83c45ada1525
Liam O'Hara <liam.ohara@gmail.com>
2024-10-28 16:59:55 +0000

    Feat: add Grid class.

---
+++ src/main/java/liamohara/model/Grid.java
@@ -0,0 +1,13 @@
+package liamohara.model;
+
+public class Grid {
+    private String[][] grid = new String[3][3];
+
+    public String[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(String[][] grid) {
+        this.grid = grid;
+    }
+}
