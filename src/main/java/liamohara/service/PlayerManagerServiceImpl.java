82fb119797dec6473f79caf32082c5b60131ed99
Liam O'Hara <liam.ohara@gmail.com>
2024-10-29 21:34:11 +0000

    Feat: add PlayerManagerServiceImpl.

---
+++ src/main/java/liamohara/service/PlayerManagerServiceImpl.java
@@ -0,0 +1,24 @@
+package liamohara.service;
+
+public class PlayerManagerServiceImpl implements PlayerManagerService {
+
+    @Override
+    public void updatePlayerMovesRemaining() {
+
+    }
+
+    @Override
+    public int getPlayerMovesRemaining() {
+        return 0;
+    }
+
+    @Override
+    public int getPlayerScore() {
+        return 0;
+    }
+
+    @Override
+    public void updatePlayerScore() {
+
+    }
+}
