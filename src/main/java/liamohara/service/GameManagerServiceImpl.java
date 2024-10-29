30b8e81a96f1bdefcec6f5da02371ac49b2c48ab
Liam O'Hara <liam.ohara@gmail.com>
2024-10-29 09:49:53 +0000

    Feat: add GameManagerServiceImpl implementing GameManagerService.

---
+++ src/main/java/liamohara/service/GameManagerServiceImpl.java
@@ -0,0 +1,21 @@
+package liamohara.service;
+
+import liamohara.model.Player;
+
+public class GameManagerServiceImpl implements GameManagerService {
+
+    @Override
+    public int getMovesRemaining() {
+        return 0;
+    }
+
+    @Override
+    public void updateMovesRemaining() {
+
+    }
+
+    @Override
+    public void setWinner(Player winner) {
+
+    }
+}
