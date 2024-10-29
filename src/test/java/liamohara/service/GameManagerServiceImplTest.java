f1347c9870288ef26441c8131d2699dad0ea0d1e
Liam O'Hara <liam.ohara@gmail.com>
2024-10-29 11:35:08 +0000

    Feat: add test for getMovesRemaining in GameManagerServiceImpl when both players have moved twice.
    Test: add test for getMovesRemaining. Existing logic sufficient to pass test.

--- src/test/java/liamohara/service/GameManagerServiceImplTest.java
+++ src/test/java/liamohara/service/GameManagerServiceImplTest.java
@@ -79,4 +79,23 @@ class GameManagerServiceImplTest {
         assertEquals(6, result);
 
     }
+
+    @Test
+    @DisplayName("Returns 2 when both players have moved twice")
+    void testGetMovesRemaining_WhenBothPlayersHaveMovedTwice() {
+
+        firstGame.setMovesRemaining(2);
+        listOfGames.add(firstGame);
+
+        when(mockGamesRepository.getListOfGames()).thenReturn(listOfGames);
+
+        int result = gameManagerServiceImpl.getMovesRemaining(firstGame.getId());
+
+        verify(mockGamesRepository, times(1)).getListOfGames();
+
+        assertEquals(2, result);
+
+    }
+
+
 }
