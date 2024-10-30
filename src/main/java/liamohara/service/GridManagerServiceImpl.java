cea959ade30b019de19fac42b350c0db7ef445fe
Liam O'Hara <liam.ohara@gmail.com>
2024-10-30 19:50:51 +0000

    Feat: add GridManagerServiceImpl, refactor and implement addNewGrid method in GridManagerService.

---
+++ src/main/java/liamohara/service/GridManagerServiceImpl.java
@@ -0,0 +1,19 @@
+package liamohara.service;
+
+public class GridManagerServiceImpl implements GridManagerService {
+
+    @Override
+    public void addNewGrid(String[][] grid, int gameId) {
+
+    }
+
+    @Override
+    public String getGrid() {
+        return "";
+    }
+
+    @Override
+    public void updateGrid(int row, int column, boolean isNought) {
+
+    }
+}
