bf078895d568c918724ce809c2f4b36ff72dd6cb
Liam O'Hara <liam.ohara@gmail.com>
2024-10-30 19:32:14 +0000

    Refactor: add gameId field, constructor with gameId parameter and getter.

--- src/main/java/liamohara/model/Grid.java
+++ src/main/java/liamohara/model/Grid.java
@@ -1,13 +1,23 @@
 package liamohara.model;
 
 public class Grid {
+
     private String[][] grid = new String[3][3];
+    private int gameId;
+
+    public Grid(int gameId) {
+        this.gameId = gameId;
+    }
 
     public String[][] getGrid() {
         return grid;
     }
 
-    public void setGrid(String[][] grid) {
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGrid(String[][] grid, int gameId) {
         this.grid = grid;
     }
 }
