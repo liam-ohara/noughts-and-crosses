2d4037ece9c70136e42967c1a65f207b3d93be56
Liam O'Hara <liam.ohara@gmail.com>
2024-11-01 14:08:57 +0000

    Feat: implement provideSingleInput and provideMultipleInputs methods in MainActivityTest to enable mocking of user inputs to the console.

--- src/test/java/liamohara/view/MainActivityTest.java
+++ src/test/java/liamohara/view/MainActivityTest.java
@@ -1,18 +1,28 @@
 package liamohara.view;
 
+import liamohara.controller.PlayerController;
 import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.ArrayList;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class MainActivityTest {
 
+
+    @Mock
+    PlayerController mockPlayerController;
+
+    @InjectMocks
     private MainActivity mainActivity = new MainActivity();
+
     private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private static final PrintStream originalOut = System.out;
 
@@ -20,6 +30,40 @@ class MainActivityTest {
     public static void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Mock
+    BufferedReader mockBufferedReader;
+
+    void provideSingleInput(String data) {
+
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+
+    }
+
+    String provideMultipleInputs(ArrayList<String> data) {
+
+        String mockConsoleInput;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < data.size(); i++) {
+            stringBuilder.append(data.get(i)).append("\n");
+
+        }
+
+        mockConsoleInput = stringBuilder.toString();
+        ByteArrayInputStream testIn = new ByteArrayInputStream(mockConsoleInput.getBytes());
+        System.setIn(testIn);
+        return mockConsoleInput;
+
+    }
+
 
     @AfterAll
     public static void restoreStreams() {
@@ -62,6 +106,4 @@ class MainActivityTest {
         assertEquals("", outContent.toString());
 
     }
-
-
 }
