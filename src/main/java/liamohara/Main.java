30818bfb90e8f5f3047c37c3413babd84b3045d9
Liam O'Hara <liam.ohara@gmail.com>
2024-11-17 11:59:45 +0000

    Feat: implement call to run method in main.java.

--- src/main/java/liamohara/Main.java
+++ src/main/java/liamohara/Main.java
@@ -1,7 +1,12 @@
 package liamohara;
 
+import liamohara.view.MainActivity;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        MainActivity mainActivity = new MainActivity();
+        mainActivity.run();
     }
 }
